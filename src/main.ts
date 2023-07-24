8ef8bd8a12c48eb2c837b7fe91ec7bdf1a6f7b83
Thiago Leite <leite.tjs@gmail.com>
2023-07-24 16:05:15 -0300

    fix cors saga 1

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,
+    origin: ['http://localhost:5173/', 'http://localhost:5173/*'],
   });
 
   app.useGlobalPipes(
