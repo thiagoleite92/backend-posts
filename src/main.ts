ce65bea23212e55d6465ca174dda54bb5b7d43ff
Thiago Leite <leite.tjs@gmail.com>
2023-07-24 16:00:35 -0300

    fix cors saga

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
