08077a8a45c1df25cd84adabb266f65a223da463
Thiago Leite <leite.tjs@gmail.com>
2023-07-24 16:13:04 -0300

    fix cors saga 2

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: ['http://localhost:5173/', 'http://localhost:5173/*'],
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
