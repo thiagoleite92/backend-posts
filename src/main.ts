3f18efb5a30006f639f9b147825004230c981bf2
Thiago Leite <leite.tjs@gmail.com>
2023-07-24 18:21:10 -0300

    fix teste cors

--- src/main.ts
+++ src/main.ts
@@ -6,20 +6,20 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-    );
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
-
-  // app.enableCors({
-  //   allowedHeaders: '*',
-  //   origin: '*',
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header(
+  //     'Access-Control-Allow-Methods',
+  //     'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+  //   );
+  //   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+  //   next();
   // });
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
